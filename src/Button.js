8b625133b107a4f7bd47952fe2b3bc301035c407
Grzegorz Patyk <70432509+grzegorzxpatyk@users.noreply.github.com>
2022-07-27 12:14:56 +0200

    playing with css transitions in Button component

--- src/Button.js
+++ src/Button.js
@@ -29,14 +29,15 @@ const Button = styled("button", {
         }
       },
       outlined: {
-        background: "transparent",
+        backgroundColor: "#ffffff00",
         backdropFilter: "blur(1px)",
         transition:
-          "transform .08s ease-in-out backdrop-filter .15s ease-in-out",
+          "transform .08s ease-in-out, backdrop-filter .15s ease-in-out, background-color 0.3s ease-in-out",
         color: "#22181c",
         border: "1px solid #11181c",
         "&:hover": {
-          backdropFilter: "blur(2px)"
+          backdropFilter: "blur(2px)",
+          backgroundColor: "#e6e6e633"
         }
       }
     }
